e4fe9f600ee258b8e3d130134aecb5b86c1ac788
moraruCristianMarian <morarucristianmarian@yahoo.com>
2024-02-29 22:04:36 +0200

    Added image uploading and gallery view
    - Added a form for uploading images to restaurant Show pages;
    - Added a gallery of images to restaurant Show pages that can be scrolled horizontally;
    - Menus and image galleries show a special message if no products/images exist.

--- LicentaApp/Controllers/ImagesController.cs
+++ LicentaApp/Controllers/ImagesController.cs
@@ -16,5 +16,29 @@ namespace LicentaApp.Controllers
             db = context;
         }
 
+
+        [HttpPost]
+        public IActionResult New(IFormFile file, string description, Guid restaurantId)
+        {
+            Restaurant rest = db.Restaurants
+                                .Where(r => r.Id == restaurantId)
+                                .First();
+
+            if ((rest == null) || (file == null))
+                return Redirect("/Restaurants/Index");
+
+            String fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            String filePath = Path.Combine("wwwroot/img", fileName);
+
+            FileStream fs = new FileStream(filePath, FileMode.Create);
+            file.CopyTo(fs);
+
+            Image image = new Image(fileName, description, DateTime.Now.ToUniversalTime(), restaurantId);
+
+            db.Images.Add(image);
+            db.SaveChanges();
+
+            return Redirect("/Restaurants/Show/" + restaurantId);
+        }
     }
 }
